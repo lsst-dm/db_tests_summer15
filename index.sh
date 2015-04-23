a2ce48126ce9ffe84afea1a5002c44cfb1e9d1d1
Serge Monkewitz <smm@ipac.caltech.edu>
2015-04-23 23:03:13 +0200

    Add HTM indexing script and config files.

---
+++ index.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+TABLE=$1
+CFG_DIR=/sps/lsst/Qserv/smm/db_tests_summer15/conf
+INDEX_DIR=/qserv/data_generation/index
+DUMP_DIR=/qserv/data_generation/dump
+
+source /qserv/stack/loadLSST.bash
+setup partition
+
+mkdir -p $INDEX_DIR
+
+sph-htm-index \
+    --config-file=$CFG_DIR/common.cfg \
+    --config-file=$CFG_DIR/$TABLE.cfg \
+    --in.csv.null=NULL \
+    --in.csv.delimiter=$'\t' \
+    --in.csv.escape=\\ \
+    --in.csv.quote='"' \
+    --in=$DUMP_DIR/$TABLE.tsv \
+    --verbose \
+    --mr.num-workers=8 \
+    --mr.pool-size=8192 \
+    --mr.block-size=16 \
+    --out.dir=$INDEX_DIR/$TABLE
